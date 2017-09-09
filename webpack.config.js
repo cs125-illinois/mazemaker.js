aa1d3df08eeb161e4c08f1bc886404c93ab61b63
Geoffrey Challen <challen@illinois.edu>
2017-09-09 18:34:41 -0500

    ES2015 transpile.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  entry: './entry.js',
+  entry: './index.js',
   output: {
     filename: 'out/mazemaker.js'
+  },
+  module: {
+    loaders: [
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['es2015']
+      }
+    }
+    ]
   }
 }
