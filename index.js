ff2262fd6cbc9a43c00d66d60bce5c1b9c77b18f
Geoffrey Challen <challen@illinois.edu>
2017-09-09 22:57:15 -0500

    Back to pure JavaScript.

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const _ = require('underscore'),
-      assert = require('chai').assert,
-      debug = require('debug')('mazemaker');
+var extend = function(obj, src) {
+  for (var key in src) {
+    if (src.hasOwnProperty(key)) obj[key] = src[key];
+  }
+  return obj;
+}
 
-'use strict';
-
-// Enumerate the movement options and the associated borders to remove
-const directions = [
-  {dx: 0, dy: 1, direction: "up" },
-  {dx: 1, dy: 0, direction: "right" },
-  {dx: 0, dy: -1, direction: "down" },
-  {dx: -1, dy: 0, direction: "left" }
-];
-const oppositeDirections = {
-  up: "down",
-  down: "up",
-  left: "right",
-  right: "left"
-};
 /**
  * Create a maze.
  * @param {Number} columnCount - number of columns in the maze
  * @param {Number} rowCount - number of rows in the maze
  * @return {Object} Object representing a maze
  */
-
-const defaults = {
-  loops: 0
-}
 var mazeMaker = function(xDimension, yDimension, config) {
-  config = _.extend(_.clone(defaults), config);
+
+  const defaults = {
+    loops: 0,
+    verbose: false
+  }
+  config = extend(defaults, config);
+
+  // Enumerate the movement options and the associated borders to remove
+  const directions = [
+  {dx: 0, dy: 1, direction: "up" },
+  {dx: 1, dy: 0, direction: "right" },
+  {dx: 0, dy: -1, direction: "down" },
+  {dx: -1, dy: 0, direction: "left" }
+  ];
+  const oppositeDirections = {
+    up: "down",
+    down: "up",
+    left: "right",
+    right: "left"
+  };
 
   // Mazes with negative dimensions or one cell are not valid
-  assert(xDimension >= 1, 'xDimension too small');
-  assert(yDimension >= 1, 'yDimension too small');
-  assert((xDimension * yDimension) > 1, 'combined dimensions too small');
+  if (xDimension < 1) {
+    throw new Error("xDimension too small");
+  }
+  if (xDimension < 1) {
+    throw new Error("yDimension too small");
+  }
+  if ((xDimension * yDimension) <= 1) {
+    throw new Error("combined dimensions too small");
+  }
 
 	// Simple maze data structure. All borders start out set and are cleared
   // below.
@@ -60,7 +69,8 @@ var mazeMaker = function(xDimension, yDimension, config) {
   for (var x = 0; x < xDimension; x++) {
     for (var y = 0; y < yDimension; y++) {
       var cell = maze[x][y];
-      _.each(directions, function (direction) {
+      for (var i = 0; i < directions.length; i++) {
+        var direction = directions[i];
         try {
           var neighbor =
             maze[cell.x + direction.dx][cell.y + direction.dy];
@@ -68,48 +78,62 @@ var mazeMaker = function(xDimension, yDimension, config) {
             cell.neighbors[direction.direction] = neighbor;
           }
         } catch (err) { };
-      });
+      }
     }
   }
 
 	// Pick a random starting point and store it to the path
-  var currentCell = maze[_.random(0, xDimension - 1)][_.random(0, yDimension - 1)];
+  var currentCell = maze[Math.floor(Math.random() * xDimension)][Math.floor(Math.random() * yDimension)]
   var path = [ currentCell ];
   var unvisitedCount = xDimension * yDimension - 1;
 
   while (unvisitedCount > 0) {
-    currentCell = _.last(path);
-    assert.notTypeOf(currentCell, 'undefined', 'current cell should not be undefined');
+    currentCell = path.slice(-1)[0];
+    if (typeof(currentCell) === 'undefined') {
+      throw new Error("current cell should not be undefined");
+    }
     currentCell.visited = true;
-    debug(`At ${currentCell.x}, ${currentCell.y}`);
+    if (config.verbose) {
+      console.log("At [" + currentCell.x + ", " + currentCell.y + "]");
+    }
 
-    var nextCells = _.chain(currentCell.neighbors)
-      .each(function (cell, direction) {
+    var nextCells = [];
+    for (direction in currentCell.neighbors) {
+      var cell = currentCell.neighbors[direction];
+      if (cell.visited === false) {
         cell.direction = direction;
-      })
-      .filter(function (cell) {
-        return cell.visited === false;
-      })
-      .value();
+        nextCells.push(cell);
+      }
+    }
 
     // If there are no neighboring cells to visit, backtrack
     if (nextCells.length === 0) {
-      debug('backtracking');
+      if (config.verbose) {
+        console.log('backtracking');
+      }
       path.pop();
       continue;
     }
 
     // Otherwise, choose a cell at random, take out the borders, mark it as
     // visited, and add it to the path
-    var nextCell = _.sample(nextCells);
+    var nextCell = nextCells[Math.floor(Math.random() * nextCells.length)];
 
-    debug(`removing ${nextCell.direction} border from [${currentCell.x}, ${currentCell.y}]`);
-    assert(currentCell.borders[nextCell.direction] === true, 'current cell should have this border');
+    if (config.verbose) {
+      console.log("removing " + nextCell.direction + " border from [" + currentCell.x + ", " + currentCell.y + "]");
+    }
+    if (currentCell.borders[nextCell.direction] !== true) {
+      throw new Error("current cell should have this border");
+    }
     currentCell.borders[nextCell.direction] = false;
 
     var oppositeBorder = oppositeDirections[nextCell.direction];
-    debug(`removing ${oppositeBorder} border from [${nextCell.x}, ${nextCell.y}]`);
-    assert(nextCell.borders[oppositeBorder] === true, 'next cell should have his border');
+    if (config.verbose) {
+      console.log("removing " + oppositeBorder + " border from [" + nextCell.x + ", " + nextCell.y + "]");
+    }
+    if (nextCell.borders[oppositeBorder] !== true) {
+      throw new Error("next cell should have his border");
+    }
     nextCell.borders[oppositeBorder] = false;
 
     unvisitedCount--;
@@ -121,20 +145,31 @@ var mazeMaker = function(xDimension, yDimension, config) {
     for (var y = 0; y < yDimension; y++) {
       var cell = maze[x][y];
       if (x === 0) {
-        assert(cell.borders.left === true);
+        if (cell.borders.left !== true) {
+          throw new Error("cell should have this border");
+        }
       }
       if (x === xDimension - 1) {
-        assert(cell.borders.right === true);
+        if (cell.borders.right !== true) {
+          throw new Error("cell should have this border");
+        }
       }
       if (y === 0) {
-        assert(cell.borders.down === true);
+        if (cell.borders.down !== true) {
+          throw new Error("cell should have this border");
+        }
       }
       if (y === yDimension - 1) {
-        assert(cell.borders.up === true);
+        if (cell.borders.up !== true) {
+          throw new Error("cell should have this border");
+        }
       }
-      _.each(cell.neighbors, function (neighbor, direction) {
-        assert(cell.borders[direction] === neighbor.borders[oppositeDirections[direction]]);
-      });
+      for (direction in cell.neighbors) {
+        var neighbor = cell.neighbors[direction];
+        if (cell.borders[direction] !== neighbor.borders[oppositeDirections[direction]]) {
+          throw new Error("mismatched borders");
+        }
+      }
     }
   }
 
@@ -150,6 +185,6 @@ var mazeMaker = function(xDimension, yDimension, config) {
   return maze;
 }
 
-exports = module.exports = global.mazeMaker = mazeMaker
+exports = module.exports = mazeMaker
 
 // vim: ts=2:sw=2:et:ft=javascript
